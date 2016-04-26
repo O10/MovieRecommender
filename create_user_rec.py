2e3a5a850b5db538c8fdbd250d4b06d480467786
Aleksander Wójcik <aleksander.wojcik@swmansion.com>
2016-04-26 12:51:29 +0200

    ref

--- create_user_rec.py
+++ create_user_rec.py
@@ -13,25 +13,25 @@ def getScore(history, similarities):
 
 print("Calculating each user top 5 recommendations")
 
-data_sims = pd.DataFrame(index=ratingTable.index,columns=ratingTable.columns)
+userSimillarities = pd.DataFrame(index=ratingTable.index, columns=ratingTable.columns)
 
-for i in range(0,len(data_sims.index)):
-    for j in range(0,len(data_sims.columns)):
-        user = data_sims.index[i]
-        product = data_sims.columns[j]
+for i in range(0, len(userSimillarities.index)):
+    for j in range(0, len(userSimillarities.columns)):
+        user = userSimillarities.index[i]
+        product = userSimillarities.columns[j]
 
         if ratingTable.iloc[i][j] > 0:
-            data_sims.iloc[i][j] = 0
+            userSimillarities.iloc[i][j] = 0
         else:
-            product_top_names = dataNeighbours.ix[product][1:10]
-            product_top_sims = simillarityMatrix.ix[product].order(ascending=False)[1:10]
-            user_purchases = ratingTable.ix[user,product_top_names]
-            data_sims.iloc[i][j] = getScore(user_purchases,product_top_sims)
+            productTopNames = dataNeighbours.ix[product][1:10]
+            productTopSims = simillarityMatrix.ix[product].order(ascending=False)[1:10]
+            userRateHistory = ratingTable.ix[user, productTopNames]
+            userSimillarities.iloc[i][j] = getScore(userRateHistory, productTopSims)
 
-dataRecommend = pd.DataFrame(index=data_sims.index, columns=range(1, 6))
+dataRecommend = pd.DataFrame(index=userSimillarities.index, columns=range(1, 6))
 
-for i in range(0,len(data_sims.index)):
-    dataRecommend.ix[i, 0:]= data_sims.iloc[i, :].order(ascending=False).iloc[0:5, ].index.transpose()
+for i in range(0, len(userSimillarities.index)):
+    dataRecommend.ix[i, 0:]= userSimillarities.iloc[i, :].order(ascending=False).iloc[0:5, ].index.transpose()
 
 dataRecommend.to_csv('data/user_recomendations.csv')
 print("Done")
